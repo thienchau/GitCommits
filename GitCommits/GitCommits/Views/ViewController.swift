7962e286c9e95a7cca4083df5d013bb513ad2e3a
Thien Chau <duythien0110@gmail.com>
2021-02-17 09:49:27 -0800

    Build the structure

--- GitCommits/GitCommits/Views/ViewController.swift
+++ GitCommits/GitCommits/Views/ViewController.swift
@@ -9,9 +9,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
+    }
+    
+    override func loadView() {
+        view = UIView()
+        view.backgroundColor = .red
     }
 
 
