99001a047dfa497364b134ccc97d2310614d29e0
Thien Chau <duythien0110@gmail.com>
2021-02-17 09:32:40 -0800

    Init project

---
+++ GitCommits/GitCommits/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GitCommits
+//
+//  Created by Duy Thien Chau on 2/17/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
